7e2cd1175eaff457af1938d106faec0b4f6e489d
anish-kumarsingh <anish.kumarsingh@wbd.com>
2025-09-02 02:34:17 +0530

    Add UI

--- ui/main_streamlite.py
+++ ui/main_streamlite.py
@@ -1,41 +1,80 @@
 import streamlit as st
-import os
 import requests
 import pandas
-import plotly.express as px
+import plotly.graph_objects as go
 
+import sys
+print(sys.executable)
 
+st.set_page_config(layout="wide")
 st.set_page_config(page_title="Welcome to AI Assistance.")
-st.title('MSC Genie')
+st.markdown("<h1 style='text-align: center;'><u>MSC Genie</u></h1>", unsafe_allow_html=True)
+
 content_available=False
 df:pandas.DataFrame = None
 sql_query:str=None
 fig = None
 
+col1, col2 = st.columns(2)
+
+
+sql_area, =st.columns([1])
+
+
 def askQuestion(query:str):
-    global sql_query
-    sql_query = requests.get(f'http://localhost:8000/generate-sql?query={query}')
-    fetchResult(sql_query=sql_query)
+    if query is not None and len(query.strip()) > 15 :
+        global sql_query
+        response = requests.get(f'http://localhost:8000/generate-sql?query={query}')
+        response.raise_for_status()
+        with sql_area:
+            st.subheader("Generated Query")
+            st.text_area("", response.json()['sql_query'], height=100)
+        df=fetchResult(sql_query=response.json()['sql_query'])
+        updateChart(df=df)
 
 def fetchResult(sql_query:str):
-    data = requests.get(f'http://localhost:8000/fetch-results?query={sql_query}')
+    response = requests.get(f'http://localhost:8000/fetch-results?query={sql_query}')
+    response.raise_for_status()
+    data=response.json()
     df=pandas.read_json(data['data'])
-    global fig
-    fig = px.bar(
-        df,
-        x=data['chart-configs']['layout']['xaxis']['title'],
-        y=data['chart-configs']['layout']['yaxis']['title'],
-        title=data['chart-configs']['layout']['title']
-    )
-    global content_available
-    content_available = True
+    with col1:
+        col1.subheader('Records:')
+        col1.dataframe(df, height=500, use_container_width=True)
+    return df
 
-with st.container:
-    if content_available : 
-        st.text_area("SQL", sql_query, height=150)
-        st.dataframe(df)
-        st.plotly_chart(fig)
-        st.text_input(placeholder="MSC Genie at your Service! Ask your query.")
-    st.button('Ask',on_click=askQuestion())
+def updateChart(df):
+    with col2:
+        st.subheader("Chart")
+        columns = df.columns.tolist()
+        x_column = st.selectbox("Select the X-axis column", columns, index=0)
+        
+        # Allow multiple columns to be selected for the Y-axis
+        y_columns = st.multiselect("Select the Y-axis columns", columns, default=[columns[1]])
 
+        # Create a Plotly Figure object
+        fig = go.Figure()
 
+        # Add a Bar trace for each dynamically selected Y-axis column
+        for y_column in y_columns:
+            fig.add_trace(go.Bar(
+                x=df[x_column],
+                y=df[y_column],
+                name=y_column
+            ))
+
+        # Update the layout with titles that reflect the selected columns
+        fig.update_layout(
+            title=f"Chart of {', '.join(y_columns)} by {x_column}",
+            xaxis_title=x_column,
+            yaxis_title="Value"
+        )
+        # Render the Plotly chart using st.plotly_chart
+        col2.plotly_chart(fig, use_container_width=True)
+        pass
+
+col_input, col_button = st.columns([0.9, 0.1])
+input_query: str=None
+with col_input:
+    input_query=st.text_input(label="Query",placeholder="MSC Genie at your Service! Ask your query.")
+with col_button:
+    st.button('Ask',on_click=askQuestion(input_query))
